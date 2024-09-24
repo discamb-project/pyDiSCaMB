d3a5c0a20fd8d9cda38cabed1720ad4324015538
Viljar Femoen <viljar.femoen@hotmail.no>
2024-09-24 09:08:02 +0200

    Minor tweaks

--- include/DiscambWrapper.hpp
+++ include/DiscambWrapper.hpp
@@ -11,6 +11,7 @@
 
 // TODO: Declare cctbx to discamb funcs for crystal
 // TODO: write throughput tests
+// TODO: avoid recalculating hkl every time
 
 enum FCalcMethod {
     IAM,
@@ -18,12 +19,10 @@ enum FCalcMethod {
 };
 
 class DiscambWrapper {
-    DiscambWrapper(discamb::Crystal crystal, std::string table) : mCrystal(crystal), mTableString(table) [];
+    DiscambWrapper(discamb::Crystal crystal, std::string table) : mCrystal(crystal), mTableString(table) {};
     DiscambWrapper(discamb::Crystal crystal) { DiscambWrapper(crystal, "electron-IT"); }
     
-    // for this one: just do name lookup (ignore atom type, just ensure label is same)
-    // also call is_crystal_valid
-    void update_crystal(const discamb::Crystal crystal);
+    void update_crystal(const discamb::Crystal &crystal);
 
     std::vector<std::complex<double>> f_calc(const double d_min, FCalcMethod method);
     std::vector<std::complex<double>> f_calc(const double d_min) { f_calc(d_min, FCalcMethod::IAM); };
@@ -31,6 +30,6 @@ class DiscambWrapper {
     private:
         discamb::Crystal mCrystal;
         std::string mTableString;
-        std::vector<discamb::Vector3i> hkl(const double d_min) const;
+        std::vector<discamb::Vector3i> get_hkl(const double d_min) const;
         bool is_crystal_valid(const discamb::Crystal crystal) const;
 };
