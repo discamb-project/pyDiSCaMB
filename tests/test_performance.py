6cc49f99b4f035c3e1d203fa358c106955e34fa5
Viljar Femoen <viljar.femoen@hotmail.no>
2024-09-25 13:59:03 +0200

    Add TAAM performance test

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -37,7 +37,7 @@ def test_update_atoms_performance(large_random_structure):
     )
 
 @pytest.mark.slow
-def test_f_calc_performance(large_random_structure):
+def test_f_calc_IAM_performance(large_random_structure):
     from time import perf_counter
     from taam_sf import DiscambWrapper
 
@@ -53,4 +53,23 @@ def test_f_calc_performance(large_random_structure):
 
     print(
         f"Runtime for {n_iter = :3_}, {len(large_random_structure.scatterers())} scatterers, {len(sf)} reflections: {end - start :.1f}s"
+    )
+
+@pytest.mark.slow
+def test_f_calc_TAAM_performance(lysosyme):
+    from time import perf_counter
+    from taam_sf import DiscambWrapper
+
+    n_iter = 100
+    d_min = 3.0
+
+    wrapper = DiscambWrapper(lysosyme)
+
+    start = perf_counter()
+    for _ in range(n_iter):
+        sf = wrapper.f_calc_TAAM(d_min)
+    end = perf_counter()
+
+    print(
+        f"Runtime for {n_iter = :3_}, {len(lysosyme.scatterers())} scatterers, {len(sf)} reflections: {end - start :.1f}s"
     )
