375fa3bb582e4b334e561af67be487af31f10d33
Viljar Femoen <viljar.femoen@hotmail.no>
2024-09-26 13:09:55 +0200

    Test runtime with bumping the atoms

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -115,3 +115,27 @@ def test_f_calc_IAM_interactive_performance(large_random_structure):
     print(
         f"Runtime for {n_iter = :3_}, {len(large_random_structure.scatterers())} scatterers, {len(sf)} reflections: {end - start :.1f}s"
     )
+
+def test_f_calc_IAM_update_structure_interactive_performance(large_random_structure):
+    from time import perf_counter
+    from random import random, choices
+    from taam_sf import InteractiveDiscambWrapper, FCalcMethod
+
+    n_iter = 100
+    d_min = 4.0
+
+    wrapper = InteractiveDiscambWrapper(large_random_structure, d_min, FCalcMethod.IAM)
+    sf_before = wrapper.f_calc()
+    runtime = 0
+    for _ in range(n_iter):
+        # Only move 20% of the scatterers each time
+        n_scatterers = len(large_random_structure.scatterers())
+        for i in choices(list(range(n_scatterers)), k=n_scatterers // 5):
+            large_random_structure.scatterers()[i].site = (random(), random(), random())
+        start = perf_counter()
+        sf_after = wrapper.f_calc()
+        runtime += perf_counter() - start
+    print(
+        f"Runtime for {n_iter = :3_}, {len(large_random_structure.scatterers())} scatterers, {len(sf_before)} reflections: {runtime :.1f}s"
+    )
+    assert not pytest.approx(sf_before) == sf_after
