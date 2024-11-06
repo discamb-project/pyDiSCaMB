8bc9844523e921b362bdc81d615e94fe2bed62d9
Viljar Femoen <viljar.femoen@hotmail.no>
2024-11-06 12:48:15 +0100

    Add table name alias for get_table

--- src/scattering_table.cpp
+++ src/scattering_table.cpp
@@ -9,23 +9,36 @@
 using namespace std;
 using namespace discamb;
 
-string GaussianScatterer::repr(){
-    stringstream out;
-    out << fixed << setprecision(2);
-    out << "GaussianScatterer(" << endl;
-    out << "    a = [";
-    for (double ai: a){
-        out << ai << ", ";
-    }
-    out << "]," << endl;
-    out << "    b = [";
-    for (double bi: b){
-        out << bi << ", ";
-    }
-    out << "]," << endl;
-    out << "    c = " << c << endl;
-    out << ")";
-    return out.str();
+
+string table_alias(string table){
+    if (table == "it") return "IT92";
+    if (table == "IT") return "IT92";
+    if (table == "it92") return "IT92";
+    if (table == "it1992") return "IT92";
+    if (table == "IT1992") return "IT92";
+    if (table == "it-92") return "IT92";
+    if (table == "it-1992") return "IT92";
+    if (table == "IT-92") return "IT92";
+    if (table == "IT-1992") return "IT92";
+
+    if (table == "wk") return "Waasmeier-Kirfel";
+    if (table == "WK") return "Waasmeier-Kirfel";
+    if (table == "wk95") return "Waasmeier-Kirfel";
+    if (table == "wk1995") return "Waasmeier-Kirfel";
+    if (table == "WK95") return "Waasmeier-Kirfel";
+    if (table == "WK1995") return "Waasmeier-Kirfel";
+    if (table == "wk-95") return "Waasmeier-Kirfel";
+    if (table == "wk-1995") return "Waasmeier-Kirfel";
+    if (table == "WK-95") return "Waasmeier-Kirfel";
+    if (table == "WK-1995") return "Waasmeier-Kirfel";
+    if (table == "Waasmeier-Kirfel95") return "Waasmeier-Kirfel";
+    if (table == "Waasmeier-Kirfel1995") return "Waasmeier-Kirfel";
+    if (table == "Waasmeier-Kirfel-95") return "Waasmeier-Kirfel";
+    if (table == "Waasmeier-Kirfel-1995") return "Waasmeier-Kirfel";
+
+    if (table == "electron") return "electron-cctbx";
+
+    return table;
 }
 
 vector<string> get_possible_entries(string atom){
@@ -45,19 +58,39 @@ vector<string> get_possible_entries(string atom){
 }
 
 map<string, GaussianScatterer> get_table(string table){
+    string alias = table_alias(table);
     map<string, GaussianScatterer> out;
     for (int z = 1; z < 120; z++){
         string atom = periodic_table::symbol(z);
         vector<string> possible_entries = get_possible_entries(atom);
         for (string possible_entry : possible_entries){
-            if (!n_gaussian_form_factors_table::hasFormFactor(possible_entry, table)){
+            if (!n_gaussian_form_factors_table::hasFormFactor(possible_entry, alias)){
                 continue;
             }
-            NGaussianFormFactor ff = n_gaussian_form_factors_table::getFormFactor(possible_entry, table);
+            NGaussianFormFactor ff = n_gaussian_form_factors_table::getFormFactor(possible_entry, alias);
             GaussianScatterer s;
             ff.get_parameters(s.a, s.b, s.c);
             out[possible_entry] = s;
         }
     }
     return out;
+}
+
+string GaussianScatterer::repr(){
+    stringstream out;
+    out << fixed << setprecision(2);
+    out << "GaussianScatterer(" << endl;
+    out << "    a = [";
+    for (double ai: a){
+        out << ai << ", ";
+    }
+    out << "]," << endl;
+    out << "    b = [";
+    for (double bi: b){
+        out << bi << ", ";
+    }
+    out << "]," << endl;
+    out << "    c = " << c << endl;
+    out << ")";
+    return out.str();
 }
--- tests/test_get_table.py
+++ tests/test_get_table.py
@@ -29,6 +29,11 @@ from pydiscamb import get_table
                 213,
                 4,
             ),
+            (
+                "empty, non-existing table",
+                0,
+                0
+            )
         ]
 )
 def test_get_table(table_str: str, expected_entries: int, expected_number_of_gaussian_parameters: int):
@@ -38,3 +43,20 @@ def test_get_table(table_str: str, expected_entries: int, expected_number_of_gau
         assert len(val.a) == expected_number_of_gaussian_parameters
         assert len(val.b) == expected_number_of_gaussian_parameters
     
+
+@pytest.mark.parametrize(
+        "table_names",
+        [
+            ("Waasmeier-Kirfel", "wk", "WK-95"),
+            ("IT92", "it1992", "IT-1992"),
+            ("electron-cctbx", "electron"),
+        ]
+)
+def test_get_table_alias(table_names):
+    tables = [get_table(name) for name in table_names]
+    for table in tables:
+        for key, val in table.items():
+            for other in tables:
+                assert other.get(key).a == val.a
+                assert other.get(key).b == val.b
+                assert other.get(key).c == val.c
