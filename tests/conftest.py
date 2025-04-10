74c252001d1907a967f56fc3035f27ed232651b7
Viljar Femoen <viljar.femoen@hotmail.no>
2025-04-10 17:46:47 +0200

    make ethane a fixture, not function

--- tests/conftest.py
+++ tests/conftest.py
@@ -133,7 +133,7 @@ def lysozyme() -> structure:
     xrs.scattering_type_registry(table="electron")
     return xrs
 
-
+@pytest.fixture
 def ethane() -> structure:
     from cctbx import crystal, xray
     from cctbx.array_family import flex
