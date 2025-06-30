6aa9aedfb823a684a46d5e341dcff03fa19927af
Viljar Femoen <viljar.femoen@hotmail.no>
2025-06-30 17:25:52 +0200

    Add stub instead of generating during build

---
+++ scripts/generate_stub.py
@@ -0,0 +1,19 @@
+# Script to generate a stub file for the C++ code
+
+from pathlib import Path
+import subprocess
+
+
+def main():
+    # Check dependency
+    import mypy
+
+    root = Path(__file__).parent.parent
+
+    subprocess.call(
+        f"stubgen -m pydiscamb._cpp_module --include-docstrings -o {root}".split()
+    )
+
+
+if __name__ == "__main__":
+    main()
