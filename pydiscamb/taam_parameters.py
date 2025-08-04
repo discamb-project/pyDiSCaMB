1b65e2e8493a53f2ae62d2fdd2d7b6cd13618997
Viljar Femoen <viljar.femoen@hotmail.no>
2025-08-04 11:35:34 +0200

    Fix "too many open files" error with databank reading

--- pydiscamb/taam_parameters.py
+++ pydiscamb/taam_parameters.py
@@ -1,20 +1,22 @@
 from pathlib import Path
 from typing import List
 
+_DEFAULT_TAAM_DATABANK: str = None
+_TAAM_ROOT: Path = None
+
 
 def get_TAAM_root() -> Path:
-    # Check if editable
-    project_root = Path(__file__).parent.parent
-    # Assume the existance of a couple files in the project
-    # means that we are in editable
-    if (
-        (project_root / "CMakeLists.txt").exists()
-        and (project_root / "pyproject.toml").exists()
-        and (project_root / "README.md").exists()
-        and (project_root / ".gitignore").exists()
-    ):
-        return project_root / "data"
-    return Path(__file__).parent / "data"
+    global _TAAM_ROOT
+    if _TAAM_ROOT is None:
+        # Check if editable
+        project_root = Path(__file__).parent.parent
+        readme = project_root / "README.md"
+        if readme.exists() and readme.read_text()[:11] == "# pydiscamb":
+            _TAAM_ROOT = project_root / "data"
+        else:
+            _TAAM_ROOT = Path(__file__).parent / "data"
+        assert _TAAM_ROOT.exists()
+    return _TAAM_ROOT
 
 
 def get_TAAM_databanks() -> List[str]:
@@ -34,10 +36,13 @@ def is_MATTS_installed() -> bool:
 
 
 def get_default_databank() -> str:
-    banks = get_TAAM_databanks()
-    search = "MATTS" if is_MATTS_installed() else "default"
-    for bank in banks:
-        if search in bank:
-            return bank
-    # Failsafe
-    return bank
+    global _DEFAULT_TAAM_DATABANK
+    if _DEFAULT_TAAM_DATABANK is None:
+        banks = get_TAAM_databanks()
+        search = "MATTS" if is_MATTS_installed() else "default"
+        for bank in banks:
+            if search in bank:
+                _DEFAULT_TAAM_DATABANK = bank
+                break
+        assert Path(_DEFAULT_TAAM_DATABANK).exists()
+    return _DEFAULT_TAAM_DATABANK
