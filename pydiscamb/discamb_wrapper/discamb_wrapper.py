8a0433e2fe32afeb8d9cb136574248573eb1aba6
Lukasz Golon <lkszgolon@gmail.com>
2025-11-14 17:52:37 +0100

    Fixed mistakes

--- pydiscamb/discamb_wrapper/discamb_wrapper.py
+++ pydiscamb/discamb_wrapper/discamb_wrapper.py
@@ -101,18 +101,16 @@ class DiscambWrapper(PythonInterface, FactoryMethodsMixin):
         )
         n_spherical_iam_water = sum(1 for k in spherical_iam if "HOH" in k)
 
-        print("Total number of atoms: ", n_atoms, file=log)
-        print("Number of atoms by assgnment:\nTAAM atom type: ", n_multipolar, file=log)
+        print("Total number of atoms:", n_atoms, file=log)
+        print("Number of atoms by assgnment:\nTAAM atom type:", n_multipolar, file=log)
         print(
-            "IAM (Slater): ",
+            "IAM (Slater):",
             n_spherical_multipolar,
-            "(in HOH: ",
-            n_spherical_multipolar_water,
-            ")",
+            "f(in HOH: {n_spherical_multipolar_water})",
             file=log,
         )
         print(
-            "IAM (Gaussian): ",
+            "IAM (Gaussian):",
             n_spherical_iam,
             # "(HOH: ",
             # n_spherical_iam_water,
